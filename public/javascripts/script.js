0f26fd3eb0acb03bcd68b9f7e84b6013d2fd1b08
aXises <leletonyli.13944@yahoo.com.au>
2017-06-18 03:51:58 +1000

    Added more effect with jQuery

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -2,6 +2,29 @@ $(document).ready(function() {
 
     var navActive;
 
+    var mainNav = $('.nav-main ul li');
+    var secNav = $('.nav-secondary ul li span');
+
+    for (var i = 0; i < mainNav.length; i++) {
+        $(mainNav[i]).attr('target', '#nav-span'+i);
+        $(secNav[i]).attr('id', 'nav-span'+i);
+    }
+    
+    $('.nav-main ul li').hover(
+        function() {
+            var target = $($(this).attr('target'))
+            target.parent().css({
+                'background-color': '#dadad2',
+            });
+            target.css('color', '#333333');
+        }, function() {
+            var target = $($(this).attr('target'))
+            target.parent().css({
+                'background-color': '',
+            });
+            target.css('color', '');
+        });
+
     $('.nav-menu, .nav-close').click(function() {
         navToggle()
     });

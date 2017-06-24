263ead8ec4adfa6bcfbcdb43b2edb97f65324c3e
aXises <leletonyli.13944@yahoo.com.au>
2017-06-24 22:05:45 +1000

    Using project.data

--- routes/gallery.js
+++ routes/gallery.js
@@ -1,12 +1,13 @@
 var express = require('express');
 var router = express.Router();
 var fs = require('fs');
-
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var data = JSON.parse(fs.readFileSync('routes/data.json', 'utf8'))
+  var content = JSON.parse(fs.readFileSync('Nikku/resources/projects/portfolio-2/project.data/projects.json', 'utf8'))
   res.render('gallery', { 
-    navLinks: data.navLinks
+    navLinks: data.navLinks,
+    pageContent: content.Projects
   });
 });
 

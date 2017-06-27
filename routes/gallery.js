ded635feb27851d5e8964ab17f509c537a2fa758
aXises <leletonyli.13944@yahoo.com.au>
2017-06-27 18:18:58 +1000

    Switched projects data path and disabled Nikku

--- routes/gallery.js
+++ routes/gallery.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var data = JSON.parse(fs.readFileSync('routes/data.json', 'utf8'))
-  var content = JSON.parse(fs.readFileSync('Nikku/resources/projects/portfolio-2/project.data/projects.json', 'utf8'))
+  var content = JSON.parse(fs.readFileSync('routes/projects.json', 'utf8'))
   res.render('gallery', { 
     navLinks: data.navLinks,
     pageContent: content.Projects

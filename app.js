dc8162f756d612e045d9ec2bc02a51a8c0b087ba
aXises <leletonyli.13944@yahoo.com.au>
2017-06-28 22:59:32 +1000

    Added item jade template

--- app.js
+++ app.js
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
 app.use('/users', users);
-app.use('/gallery', index);
+app.use('/Works', index);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {

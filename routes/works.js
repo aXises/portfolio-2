257610a2836847744846deba893130787f8c859d
aXises <leletonyli.13944@yahoo.com.au>
2017-07-08 20:43:52 +1000

    Added methods for processing new db format

--- routes/works.js
+++ routes/works.js
@@ -4,43 +4,64 @@ var jade = require('jade');
 var database = require('../database');
 
 /* GET works page. */
-router.get('/', function(req, res, next) {
-  database.getCollection('data', function(data) {
-    res.render('works', { 
-      pageContent: data.Projects,
-      itemKeys: Object.keys(data.Projects)
+database.connectDb(function(err) {
+  var db = database.getDb();
+  router.get('/', function(req, res, next) {
+    db.collection('items').find({}).toArray(function(err, result) {
+      if (err) throw err;
+      var items = result;
+      var data = {};
+      var keys = Object.keys(items);
+      for (var i = 0; i < items.length; i++) {
+        var itemKey = Object.keys(items[i])[1];
+        var item = items[i][itemKey];
+        data[itemKey] = item;
+      };
+      res.render('works', { 
+        pageContent: data,
+        itemKeys: Object.keys(data)
+      });
     });
   });
-});
 
-router.get('/:item', function(req, res, next) {
-  database.getCollection('data', function(data) {
-    var extension;
-    if (data.Projects[req.params.item.toUpperCase()]['Extended']) {
-      extension = jade.renderFile('views/extensions/'+req.params.item+'.extended.jade');
-    }
-    res.render('item', {
-      pageContent: data.Projects,
-      itemKeys: Object.keys(data.Projects),
-      itemID: req.params.item,
-      extension: extension
-    }, function(err, page) {
-      if (err) {
-        console.log('error', err);
-        var err;
-        if (req.params.item.replace(/[0-9]/g, '') === "item") {
-          err = new Error('NotImplementedError');
-          err.status = 404;
+  router.get('/:item', function(req, res, next) {
+    db.collection('items').find({}).toArray(function(err, result) {
+      if (err) throw err;
+      var items = result;
+      var data = {};
+      var keys = Object.keys(items);
+      for (var i = 0; i < items.length; i++) {
+        var itemKey = Object.keys(items[i])[1];
+        var item = items[i][itemKey];
+        data[itemKey] = item;
+      }
+      var extension;
+      if (data[req.params.item.toUpperCase()]['Extended']) {
+        extension = jade.renderFile('views/extensions/'+req.params.item+'.extended.jade');
+      }
+      res.render('item', {
+        pageContent: data,
+        itemKeys: Object.keys(data),
+        itemID: req.params.item,
+        extension: extension
+      }, function(err, page) {
+        if (err) {
+          console.log('error', err);
+          var err;
+          if (req.params.item.replace(/[0-9]/g, '') === "item") {
+            err = new Error('NotImplementedError');
+            err.status = 404;
+          }
+          else {
+            err = new Error('UndefinedPage');
+            err.status = 404;
+          }
+          next(err);
         }
         else {
-          err = new Error('UndefinedPage');
-          err.status = 404;
+          res.end(page);
         }
-        next(err);
-      }
-      else {
-        res.end(page);
-      }
+      });
     });
   });
 });

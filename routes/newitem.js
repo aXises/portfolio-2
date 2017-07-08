2e8b191a28262780545352f5e64cd774831d8c25
aXises <leletonyli.13944@yahoo.com.au>
2017-07-08 20:41:31 +1000

    Form now generates from template object and added request handlers

--- routes/newitem.js
+++ routes/newitem.js
@@ -3,33 +3,31 @@ var router = express.Router();
 var jade = require('jade');
 var database = require('../database');
 
-router.post('/', function(req, res, next) {
-    console.log(req.method)
-    var keys = Object.keys(req.body)
-    for (var i = 0; i < keys.length; i++) {
-      // console.log(keys[i], req.body[keys[i]])
-      var value = req.body[keys[i]]
-    }
-    //console.log(req.body)
-    database.getCollection('data', function(data) {
-      console.log('current data size', Object.keys(data.Projects).length)
-      var itemSize = Object.keys(data.Projects).length
-      var projects = [data.Projects]
-      var item = {}
-      item['ITEM'+(itemSize+1)] = req.body
-      projects.push(item)
-      console.log(projects)
+database.connectDb(function(err) {
+  if (err) throw err;
+  var db = database.getDb();
+  router.post('/', function(req, res, next) {
+    db.collection('items').find({}).toArray(function(err, result) {
+      var items = result;
+      var itemSize = items.length;
+      var item = {};
+      item['ITEM'+(itemSize+1)] = req.body;
+      db.collection('items').insert(item)
+      res.redirect('/newitem');
     });
+  });
 
-    return false;
-});
-
-router.get('/', function(req, res, next) {
-  database.getCollection('data', function(data) {
-    res.render('newitem', { 
-      itemKeys: Object.keys(data.Projects),
-      template: data.Templates["Project"],
-      templateKeys: Object.keys(data.Templates["Project"])
+  router.get('/', function(req, res, next) {
+    db.collection('dataTemplates').findOne(function(err, result) {
+      var templates = result;
+      db.collection('items').find({}).toArray(function(err, result) {
+        var items = result;
+        res.render('newitem', { 
+          itemKeys: Object.keys(items),
+          template: templates["Project"],
+          templateKeys: Object.keys(templates["Project"])
+        });
+      });
     });
   });
 });

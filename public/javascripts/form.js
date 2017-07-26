dadd537e76dcf8d6f580f71580351f0c942da802
aXises <leletonyli.13944@yahoo.com.au>
2017-07-26 18:28:34 +1000

    Added method to insert data in to fields

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -1,7 +1,4 @@
 $(document).ready(function() {
-    $('form').submit(function(event) {
-
-    });
     $('.newsub').click(function(event) {
         event.preventDefault();
         var newField = $('#' + $(this).attr('attr')).clone();
@@ -16,8 +13,27 @@ $(document).ready(function() {
             data: {'name':$(this).attr('attr'), 'index': $(this).attr('item')},
             type: "POST",
             success: function (data) {
-                console.log(data)
+                insertFields(data);
             }
         });
     });
 });
+
+function insertFields(data) {
+    var dataKeys = Object.keys(data);
+    for (var i = 0; i < dataKeys.length; i++) {
+        if (typeof(data[dataKeys[i]]) === 'string') {
+            var field = $('form').find('[name=' + dataKeys[i] + ']');
+            field.val(data[dataKeys[i]]);
+        }
+        else if (typeof(data[dataKeys[i]]) === 'object') {
+            var keys = Object.keys(data[dataKeys[i]]);
+            var subData = data[dataKeys[i]];
+            for (var j = 0; j < keys.length; j++) {
+                var name = '[name="sub' + keys[j] + ',' + dataKeys[i] + '"]';
+                var field = $('form').find(name);
+                field.val(subData[keys[j]]);
+            }
+        }
+    }
+}

c0df11629b1b238f98fbc324e3a776313f05ab92
aXises <leletonyli.13944@yahoo.com.au>
2017-07-26 21:36:20 +1000

    Keep functions in scope

--- public/javascripts/form.js
+++ public/javascripts/form.js
@@ -15,36 +15,37 @@ $(document).ready(function() {
             }
         });
     });
-});
 
-function addField(field) {
-    var target = 'form .'+ field;
-    var newField = $(target).clone();
-    newField.attr('name', field)
-    newField.attr('class', field + $('form input[name="'+ field + '"]').length)
-    newField.val('');
-    $(target).parent().append(newField);
-}
+    function addField(field) {
+        var target = 'form .'+ field;
+        var newField = $(target).clone();
+        newField.attr('name', field)
+        newField.attr('class', field + $('form input[name="'+ field + '"]').length)
+        newField.val('');
+        $(target).parent().append(newField);
+    }
 
-function insertFields(data) {
-    var dataKeys = Object.keys(data);
-    for (var i = 0; i < dataKeys.length; i++) {
-        if (typeof(data[dataKeys[i]]) === 'string') {
-            if (typeof(data[dataKeys[i]]) === 'object') {
-
-            } else {
-                var field = $('form').find('[name=' + dataKeys[i] + ']');
-                field.val(data[dataKeys[i]]);
+    function insertFields(data) {
+        var dataKeys = Object.keys(data);
+        for (var i = 0; i < dataKeys.length; i++) {
+            if (typeof(data[dataKeys[i]]) === 'string') {
+                if (typeof(data[dataKeys[i]]) === 'object') {
+                    
+                } else {
+                    var field = $('form').find('[name=' + dataKeys[i] + ']');
+                    field.val(data[dataKeys[i]]);
+                }
             }
-        }
-        else if (typeof(data[dataKeys[i]]) === 'object') {
-            var keys = Object.keys(data[dataKeys[i]]);
-            var subData = data[dataKeys[i]];
-            for (var j = 0; j < keys.length; j++) {
-                var name = '[name="sub' + keys[j] + ',' + dataKeys[i] + '"]';
-                var field = $('form').find(name);
-                field.val(subData[keys[j]]);
+            else if (typeof(data[dataKeys[i]]) === 'object') {
+                var keys = Object.keys(data[dataKeys[i]]);
+                var subData = data[dataKeys[i]];
+                for (var j = 0; j < keys.length; j++) {
+                    var name = '[name="sub' + keys[j] + ',' + dataKeys[i] + '"]';
+                    var field = $('form').find(name);
+                    field.val(subData[keys[j]]);
+                }
             }
         }
     }
-}
+
+});

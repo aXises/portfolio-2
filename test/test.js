83347f2be2cf8bef210e1a4092caf30529bbd734
Toe, Knee <leletonyli.13944@yahoo.com.au>
2017-07-31 19:39:12 +1000

    Testing mongo db connection

--- test/test.js
+++ test/test.js
@@ -3,6 +3,7 @@ var assert = require('assert');
 var origin = "http://localhost:3000/"
 var http = require('http');
 var app = require('../app');
+var database = require('../database');
 
 var server = http.createServer(app);
 server.listen("3000");
@@ -14,7 +15,18 @@ describe("App", function() {
         assert.equal(200, res.statusCode);
         server.close();
         done();
+before(function(done) {
+  this.timeout(20000);
+  database.connectDb(function () {
+    var db = database.getDb()
+    describe('MongoDB', function() {
+      describe('Database connection', function() {
+        it('Connects to Mongo Atlas', function(done) {
+          assert.ok(db.serverConfig.isConnected());
+          done();
+        });
       });
     });
+    done();
   });
 });

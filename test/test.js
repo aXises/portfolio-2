7918c31d4ef9a9eac70c606f576f682f78303052
Toe, Knee <leletonyli.13944@yahoo.com.au>
2017-07-31 19:33:57 +1000

    Revert "Added methods to get links"
    This reverts commit b38f74d23e23a1d1301a803e83ed0e6f442de558.

--- test/test.js
+++ test/test.js
@@ -11,22 +11,11 @@ server.listen("3000");
 describe("App", function() {
   describe("GET /", function() {
     it("returns status code 200", function(done) {
-      var links = getLinks(origin);
-
-      server.close();
+      request.get(origin, function(err, res) {
+        assert.equal(200, res.statusCode);
+        server.close();
+        done();
+      });
     });
   });
 });
-
-function getLinks(page) {
-  request.get(page, function (err, res, body) {
-    if (err) throw err;
-    $ = cheerio.load(body);
-    var links = $('nav a');
-    var urls = [page];
-    for (var i = 0; i < links.length; i++) {
-      urls.push(page + $(links[i]).attr('href').substr(1));
-    }
-    return urls;
-  });
-}

d168efd4e45fd486fa5e916a7c28605457783379
Toe, Knee <leletonyli.13944@yahoo.com.au>
2017-08-06 02:08:15 +1000

    Added function to get specific type of items

--- routes/index.js
+++ routes/index.js
@@ -17,4 +17,19 @@ database.connectDb(function(err) {
   });
 });
 
+function getItem(data, status) {
+  var sortedData = data.sort(function(a,b) {
+    a = a.Date.split('/').reverse().join('');
+    b = b.Date.split('/').reverse().join('');
+    return a > b ? 1 : a < b ? -1 : 0;
+  });
+  sortedData = sortedData.reverse();
+  for (var i = 0; i < sortedData.length; i++) {
+    if (sortedData[i].Status.toLowerCase() === status) {
+      console.log(sortedData[i]);
+      return sortedData[i];
+    }
+  }
+}
+
 module.exports = router;

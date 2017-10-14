e1b5b374be5a1187e2dfcca82a5849a31cc44459
Toe, Knee <leletonyli.13944@yahoo.com.au>
2017-10-14 17:24:17 +1000

    Set route envrionment

--- app.js
+++ app.js
@@ -12,9 +12,11 @@ var app = express();
 // routes setup
 var index = require('./routes/index');
 var users = require('./routes/users');
-//var works = require('./routes/works');
 var portfolio = require('./routes/portfolio');
 var showcases = require('./routes/showcases');
+var itemData = require('./routes/itemdata');
+var collectionData = require('./routes/collectiondata');
+var teamData = require('./routes/teamdata');
 
 // middleware setup
 app.use(lessMiddleware(__dirname + '/public'));
@@ -40,9 +42,6 @@ app.use('/showcases', showcases);
 
 // set environment
 if (app.get('env') === 'development') {
-  var itemData = require('./routes/itemdata');
-  var collectionData = require('./routes/collectiondata');
-  var teamData = require('./routes/teamdata');
   app.use('/itemdata', itemData);
   app.use('/collectiondata', collectionData);
   app.use('/teamdata', teamData);
